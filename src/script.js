bbc7ca96a0eced0ea976acb4a90c7c89164d2067
Dicky Ramadhan <dikivenom@gmail.com>
2025-02-06 14:26:51 +0700

    Membuat Navbar

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger')
+const mobileNav = document.getElementById('mobile-nav')
+
+hamburger.addEventListener('click', function(){
+    
+    mobileNav.classList.toggle('hidden')
+})
